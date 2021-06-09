2709ebbefe143ac0f50e4075f45284acc18d8453
Madi <72963376+Madi-S@users.noreply.github.com>
2021-06-09 19:07:14 +0600

    components destructurized

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -2,10 +2,22 @@ import {ExcelComponent} from '@/core/ExcelComponent'
 
 export class Formula extends ExcelComponent {
     static className = 'excel__formula' 
+    static name = 'Formula'
+
+    constructor($root) {
+        super($root, {
+            name: Formula.name,
+            listeners: ['input', 'click']
+        })
+    }
 
     toHTML() {
         return `
         <div class="info">Fx</div>
         <div class="input" contenteditable spellcheck="false"></div>`
+    }
+
+    onInput(event) {
+        console.log(Formula.name, 'onInput', event)
     }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,8 +4,17 @@ import {
 
 export class ExcelComponent extends DOMListener {
 
+    constructor($root, options = {}) {
+        super($root, options.listeners)
+    }
+
     // Returns component's HTML template
     toHTML() {
         return ''
+    }
+
+    // Must be called after HTML DOM is rendered
+    init() {
+        this.initDOMListeners()
     }
 }
