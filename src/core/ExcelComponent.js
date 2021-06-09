bad638e37d355a20423ca12c5dc38d92c2e3a84c
Madi <72963376+Madi-S@users.noreply.github.com>
2021-06-09 13:32:52 +0600

    framework structure created

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,11 @@
+import {
+    DOMListener
+} from '@core/DOMListener'
+
+export class ExcelComponent extends DOMListener {
+
+    // Returns component's HTML template
+    toHTML() {
+        return ''
+    }
+}
