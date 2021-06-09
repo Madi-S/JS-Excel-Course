cf4c99a233c1bcd338564ec367e4be5c1d8289ec
Madi <72963376+Madi-S@users.noreply.github.com>
2021-06-09 14:45:19 +0600

    excel render abstract method defined

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,19 @@ export class Excel {
         this.$el = document.querySelector(selector)
         this.components = options.components || []
     }
+
+    getRoot() {
+        const $root = document.createElement('div')
+        $root.classList.add('excel')
+        this.components.forEach(Component => {
+            const component = new Component()
+            $root.insertAdjacentHTML('beforeend', component.toHTML())
+        })
+        return $root
+    }
+
+    render() {
+        // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
+        this.$el.append(this.getRoot())
+    }
 }
