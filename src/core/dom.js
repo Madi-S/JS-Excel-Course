25f94c973eb4e0a2728e7e2eec663d08596f538a
Madi <72963376+Madi-S@users.noreply.github.com>
2021-06-09 16:12:07 +0600

    additional $ methods added

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,19 +1,54 @@
 class Dom {
-    constructor() {
+    constructor(selector) {
+        // #app
+        this.$el = typeof selector === 'string' ?
+            document.querySelector(selector) :
+            selector
+    }
 
+    html(html) {
+        if (typeof html === 'string') {
+            this.$el.innerHTML = html
+            return this
+        }
+        return this.$el.outerHTML.trim()
+    }
+
+    clear() {
+        this.html('')
+        return this
+    }
+
+    on() {
+        
+    }
+
+    append(node) {
+        if (node instanceof Dom) {
+            node = node.$el
+        }
+
+        if (Element.prototype.append) {
+            this.$el.append(node)
+        } else {
+            this.$el.appendChild(node)
+        }
+        return this
     }
 }
 
-export function $() {
-    return new Dom()
+// event.target
+export function $(selector) {
+    return new Dom(selector)
 }
 
+
+$('div').html('<h1>Test</h1>').clear()
 
 $.create = (tagname, classes = '') => {
     const el = document.createElement(tagname)
-    // classes.forEach(className => el.classList.add(className))
     if (classes) {
         el.classList.add(classes)
     }
-    return el 
+    return $(el)
 }
